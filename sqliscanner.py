0f757e6727c2a024cf947847b42c1866744c9fb7
Alex <alex.thomas@trojans.dsu.edu>
2019-06-06 12:07:46 -0400

    Update sqliscanner.py

--- sqliscanner.py
+++ sqliscanner.py
@@ -1,6 +1,10 @@
 import requests
 import json
 import re
+
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
 class sqliScannerClass():
 
     def __init__(self):
